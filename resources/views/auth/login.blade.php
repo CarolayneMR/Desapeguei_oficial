f43e0a1cd41762a3c046b7b06e789ae845d7c350
Dudu200313 <eduardo.silvinocabuloso23@gmail.com>
2023-04-26 17:55:22 -0300

    Protótico da dela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            
         </x-slot>
 
         <x-validation-errors class="mb-4" />
@@ -16,31 +16,28 @@
             @csrf
 
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
+                <x-label for="email" value="{{ __('Usuário') }}" />
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-label for="password" value="{{ __('Senha') }}" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="senha" required autocomplete="current-password" />
+                 
+  </button>
             </div>
 
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-1 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-button class="ml-4">
-                    {{ __('Log in') }}
+            <div class="bg-blue-350 grid grid-cols-1  p-2 px-4 rounded">
+               
+                <x-button  class="bg-blue-350 grid grid-cols-1  p-2 px-4 rounded" >
+                    {{ __('Entrar') }}
                 </x-button>
             </div>
         </form>
