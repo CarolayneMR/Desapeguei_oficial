4364c807643cb00431153dfb6ec9be8a2d95c39c
Mateus Lopes <mateusyasmim2014@gmail.com>
2023-04-30 22:54:18 -0300

    Fazendo com que apenas os objetos do usuário logado sejam exibidos na dashboard

--- resources/views/objetos/create.blade.php
+++ resources/views/objetos/create.blade.php
@@ -8,19 +8,27 @@
         <form action="{{ route('objetos.store') }}" method="POST">
             @csrf
             <div>
-                <label for="descricao" :value="__('Objeto descricao')" />
-                <input id="descricao" class="block mt-1 w-full" type="text" name="descricao" required autofocus />
+                <x-label for="nome" :value="__('Nome')" />
+                <input id="nome" class="block mb-2 w-full" type="text" name="nome" required autofocus />
+            </div>
+            <div>
+                <x-label for="descricao" :value="__('Descrição')" />
+                <input id="descricao" class="block mb-2 w-full" type="text" name="descricao" required autofocus />
+            </div>
+            <div>
+                <x-label for="cep" :value="__('CEP')" />
+                <input id="cep" class="block mb-2 w-full" type="text" name="cep" required autofocus />
             </div>
             <div> 
-                <label for="tipo" :value="__('Objeto tipo')" />
-                <select name="tipo" id="tipo">
+                <x-label for="tipo" :value="__('Tipo de objeto')" />
+                <select name="tipo" id="tipo" class="mb-2">
                     @foreach (\App\Models\Tipo::all() as $tipo)
                         <option value="{{ $tipo->id }}">{{ $tipo->descricao }}</option>
                     @endforeach
                 </select>
             </div>
             <div>
-                <button class="bg-red-500 hover:bg-red-700 font-bold py-2 px-4 rounded">Fazer doação</button>
+                <button class="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded">Fazer doação</button>
             </div>
         </form>
     </div>
