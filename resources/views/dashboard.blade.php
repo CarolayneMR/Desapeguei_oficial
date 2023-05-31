fafca338061bc463ee0fca392deeb4048729c86a
Suelen Salvino <suelensalvinocs@gmail.com>
2023-05-31 17:42:20 -0300

    Alterando variavel de tipo para descrição no card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,7 @@
                                 <br>
                                 CEP do doador: {{ $objeto->cep }}
                                 <br>
-                                Tipo do objeto: {{ $objeto->tipo_id }}
+                                Tipo do objeto: {{ $objeto->tipos->descricao }}
                             </p>
                             <a href="/objetos/{{ $objeto->id }}"
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
