5376fe1f6d5875bb6029fd84c61821d63ee005d9
ianq1w1 <ianpereiraian2@gmail.com>
2023-06-04 00:28:28 -0300

    adição de restrições ao agendamento

--- app/Http/Controllers/AgendaController.php
+++ app/Http/Controllers/AgendaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Objeto;
 use App\Models\Agenda;
+use App\Http\Controllers\Log;
 
 class AgendaController extends Controller
 {
@@ -17,16 +18,19 @@ class AgendaController extends Controller
 
     public function store(Request $request, $id){
         $objeto = Objeto::find($id);
-
-        Agenda::create([
-            'data' => $request->data,
-            'usuarioDest_id' => $request->user()->id,
-            'usuarioDoar_id' => $objeto->user_id,
-            'objeto_id' => $objeto->id,
-            'status' => 'aberto'
-        ]);
-
+        if($objeto->user_id != $request->user()->id){
+            Agenda::create([
+                'data' => $request->data,
+                'usuarioDest_id' => $request->user()->id,
+                'usuarioDoar_id' => $objeto->user_id,
+                'objeto_id' => $objeto->id,
+                'status' => 'aberto'
+            ]);
+        
         return redirect(route('agenda.index'));
+        }else{
+            echo "irmao tu nao pode doar pra si mesmo vacilao";
+        }
     }
     public function update(Request $request, Agenda $agenda)
     {
