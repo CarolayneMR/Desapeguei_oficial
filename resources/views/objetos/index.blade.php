598e33bd5095fbacc1abffa0c89d730ac562de1c
Suelen Salvino <suelensalvinocs@gmail.com>
2023-06-14 16:24:56 -0300

    Alterando modal de exclusão

--- resources/views/objetos/index.blade.php
+++ resources/views/objetos/index.blade.php
@@ -123,7 +123,7 @@
                         </div>
                         <div x-show="showDelete"
                             class="absolute top-0 bottom-0 left-0 right-0 bg-gray-900 bg-opacity-20 z-0">
-                            <div class="md:w-96 bg-gray-800 p-4 absolute left-1/3 right-1/3 top-1/4 z-50">
+                            <div class="md:w-96 bg-white p-4 relative rounded-lg left-1/3 right-1/3 top-1/4 z-50">
                                 <form class="py-2 w-full text-center"
                                     action="{{ route('objetos.destroy', $objeto) }}" method="POST">
                                     @csrf
@@ -139,22 +139,21 @@
                                         </svg>
                                         <span class="sr-only">Fechar</span>
                                     </button>
-                                    <svg class="text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
+                                    <svg class="text-black dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto"
                                         aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                                         xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd"
                                             d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                                             clip-rule="evenodd"></path>
                                     </svg>
-                                    <p class="mb-4 text-gray-500 dark:text-gray-300">Tem certeza que deseja excluir
-                                        este
-                                        objeto?</p>
+                                    <p class="mb-4 text-black">Tem certeza que deseja excluir
+                                        este objeto?</p>
                                     <button @click="showDelete = true"
                                         class="w-full py-2 px-3 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900">Sim,
                                         tenho certeza</button>
                                 </form>
                                 <button
-                                    class="w-full py-2 px-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                                    class="w-full py-2 px-3 text-sm font-medium text-gray-800 bg-transparent rounded-lg border border-gray-500 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
                                     @click="showDelete = false">Não, cancelar</button>
                             </div>
                         </div>
