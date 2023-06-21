816b9c632d5e4f7586bfdcab15f1fb2df617effa
Suelen Salvino <suelensalvinocs@gmail.com>
2023-06-21 17:42:45 -0300

    Alterando cor do card em meus objetos

--- resources/views/objetos/index.blade.php
+++ resources/views/objetos/index.blade.php
@@ -14,15 +14,15 @@
                             <template x-if="!editMode">
                                 <span>
                                     <div
-                                        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                        class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
                                         <img class="md:w-full" src="/img/objetos/{{ $objeto->imagem }}"
                                             alt="Imagem do objeto" />
                                         </a>
                                         <div class="p-5">
                                             <h5
-                                                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                                                 {{ $objeto->nome }} </h5>
-                                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                            <p class="mb-3 font-normal text-gray-700">
                                                 Descrição: {{ $objeto->descricao }}
                                                 <br>
                                                 CEP do doador: {{ $objeto->cep }}
