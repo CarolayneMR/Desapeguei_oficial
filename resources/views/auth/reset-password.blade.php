3aad498624ccd1d1c4590bff34a394bd26776659
Dudu200313 <eduardo.silvinocabuloso23@gmail.com>
2023-06-27 23:40:45 -0300

    Adicionando front da tela de Redefinir senha

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,13 @@
 <x-guest-layout>
+<div class="grid grid-cols-2 gap-0 m-auto">
+        <div class="bg-blue-500">
+            <div class="font-bold text-5xl m-5 flex">
+                <img class="h-20 w-20" src="https://raw.githubusercontent.com/CarolayneMR/Desapeguei-v2/7ace94c4ef6f5ebfcc9c5050e02d0419f4215662/resources/views/assets/img/logo-desapeguei.png">
+                <div class="ml-5 m-auto">Desapeguei!</div>
+            </div>
+
+            <img class="m-auto" src="https://raw.githubusercontent.com/CarolayneMR/Desapeguei-v2/main/resources/views/assets/img/reset-password.png" alt="Um cadeado .">
+        </div>
     <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
@@ -17,18 +26,18 @@
             </div>
 
             <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
+                <x-label for="password" value="{{ __('Senha Nova') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                <x-label for="password_confirmation" value="{{ __('Confirmar Senha') }}" />
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-center mt-4">
                 <x-button>
-                    {{ __('Reset Password') }}
+                    {{ __('Redefinir Senha') }}
                 </x-button>
             </div>
         </form>
